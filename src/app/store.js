ce0d1f5c0bae2e7e5244c76ce2981e99388b98f3
thrasher <wickynjy@gmail.com>
2022-12-23 13:02:12 -0600

    Added PostsList: Can now display posts in the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import postsReducer from '../features/posts/postsSlice'
+// Here, we are telling redux we want our top-level state object to have a field named
+// posts inside, and all the data for state.posts will be updated by the
+// postsReducer function when actions are dispatched.
 
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts: postsReducer
+  }
 })
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,4 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 // ----------------- Main Posts Feed --------------------------------
         // -The main feature of the social media feed app will be a list of posts
 
-// The first step is to create a new Redux slice that will contain data for our psots.
+// The first step is to create a new Redux slice that will contain data for our posts
+// We'll import createSlice, define the initial posts array, and pass thatt
+// to createSlice, and export the posts reducer function that createSlice created for us
+const initialState = [
+    { id: '1', title: 'First Post!', content: 'Hello!' },
+    { id: '2', title: 'Second Post', content: 'More text' }
+]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {}
+})
+
+export default postsSlice.reducer
+
+// Every time we create a new slice, we need to add its reducer function to our Redux store.
+// Since we already hav a redux store without any data, we'll update the call to configureStore
+// by passing postsReducer function as a reducer field named posts in our app/store.js
