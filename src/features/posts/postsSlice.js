a9907057c3bb9d3955f89003cdda180b5d19c98c
thrasher <wickynjy@gmail.com>
2022-12-23 12:39:41 -0600

    Added postsSlice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+// ----------------- Main Posts Feed --------------------------------
+        // -The main feature of the social media feed app will be a list of posts
+
+// The first step is to create a new Redux slice that will contain data for our psots.
